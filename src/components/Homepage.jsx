d26a5b70287d64033537914c5d20cccf78c56329
HAXAZE <28skofficial01@gmail.com>
2023-09-19 17:24:25 +0530

    Made Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import millify from "millify";
+import {Typography,Row,Col,Statistic} from  "antd";
+import {Link} from "react-router-dom";
+const {Title}   = Typography;
+ 
 const Homepage = () =>{
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+        <Title level={2} className="heading">Global Crypto Stats</Title>
+        <Row>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+
+        </Row>
+        </>
     )
 }
 
