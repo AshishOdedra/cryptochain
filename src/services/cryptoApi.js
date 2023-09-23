6cbad03b3af6e3bfd12dd62568e379c151c287c9
HAXAZE <28skofficial01@gmail.com>
2023-09-23 19:45:22 +0530

    make changes in api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,7 @@
 import  {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
 
 const cryptoApiHeaders = {
-      'X-RapidAPI-Key': '07c4386213msh2c5699b2ac39d38p144e3bjsn7c2af28c64c9',
-      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+      
 }
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
